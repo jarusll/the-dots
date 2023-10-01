3979970174cc4d5db980a400348dae1092576fa6
Suraj Yadav <ysuraj877@gmail.com>
2023-10-01 20:57:00 +0530

    Add alias & env vars to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,10 @@
 # init startup prompt
 starship init fish | source
 
+# env vars
 set fish_greeting ""
+set NVM_DIR "/usr/local/nvm"
+
+# aliases
+alias vifish "vi ~/.config/fish/config.fish"
+alias reload-fish "source ~/.config/fish/config.fish"
